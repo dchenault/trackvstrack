ebeaab1a65787c9668458812144a79f1d433a3fc
Dave Chenault <webschemes.net@gmail.com>
2026-02-11 07:48:11 +0000

    Why are you asking to create a new firebase project?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 // The following config will be replaced by the Studio with your project's config.
 // This is not a secret, and is safe to be publicly exposed.
 export const firebaseConfig = {
-  apiKey: "AIzaSyDP4374N1sduLJ8ryuahG_Bm2TNe6FuCbg",
+  apiKey: "AIzaSyDLQeCck1I5SO5BECF1szzgjyAO3Vm4Dss",
   authDomain: "studio-7633580652-c2695.firebaseapp.com",
   projectId: "studio-7633580652-c2695",
   storageBucket: "studio-7633580652-c2695.appspot.com",
