4c571a9a9bbbde48ca110fbbd7259786e4fe8601
Dave Chenault <webschemes.net@gmail.com>
2026-02-11 00:49:20 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // The following config will be replaced by the Studio with your project's config.
 // This is not a secret, and is safe to be publicly exposed.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDLQeCck1I5SO5BECF1szzgjyAO3Vm4Dss",
+  authDomain: "studio-7633580652-c2695.firebaseapp.com",
+  projectId: "studio-7633580652-c2695",
+  storageBucket: "studio-7633580652-c2695.appspot.com",
+  messagingSenderId: "647533949543",
+  appId: "1:647533949543:web:18e97496b75b50a99f1ac2",
 };
